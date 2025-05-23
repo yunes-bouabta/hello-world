705a48494eb320bf9ece18f10f0b15307781db54
BOUABTA Yunes <12401522@s30423.iutv.univ-paris13.fr>
2025-05-23 16:01:25 +0200

    modif hello world en ola mundo

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Ciao mondo !");
-		System.out.println("Hello World !");
+		System.out.println("Ola mundo !");
 		System.out.println("Hueco Mundo !");
 	}
 
